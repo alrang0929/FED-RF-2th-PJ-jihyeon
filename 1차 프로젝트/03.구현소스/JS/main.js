193138d1f2c7d754d3e266eee36c231f4c13994d
ParkSuMin0611 <71934605+ParkSuMin0611@users.noreply.github.com>
2024-03-20 15:22:27 +0900

    asd

--- 1차 프로젝트/03.구현소스/JS/main.js
+++ 1차 프로젝트/03.구현소스/JS/main.js
@@ -10,13 +10,23 @@ aa.forEach((ele,idx)=>{
   }
 })
 
-/* 썸네일 리스트 */
-let arrow = document.querySelectorAll('.nlist span');
-let thumblist = document.querySelector('.nlist ul');
-arrow.forEach((ele,idx)=>{
-  ele.onclick=(e)=>{
-    e.preventDefault();
-    thumblist.style.left = (-100*idx)+"%";
-    thumblist.style.transition = '.6s';
-  }
-})
+
+/* 리스트 박스 */
+const nlist = document.querySelector('.nlist')
+
+/* 리스트 내용물*/
+const nimg = document.querySelectorAll('.product-main a')
+
+
+//뒤로가기 화살표
+const prevBtn = document.querySelector('#prevBtn')
+
+//다음이동 화살표
+const nextBtn = document.querySelector('#nextBtn')
+
+/* 1페이지에 보이는 이미지 수? */
+let counter = 4;
+
+/* 리스트 이미지로 가로값 지정 */
+const size = nimg[0].clientWidth;
+nimg.style.transform = 'translateX(' + (-size * counter) + 'px)';
