4d72532c81dfd277f5c04c5d9b3e70ea8370af7b
ParkSuMin0611 <71934605+ParkSuMin0611@users.noreply.github.com>
2024-03-22 17:07:48 +0900

    asd

--- 1차 프로젝트/03.구현소스/JS/main.js
+++ 1차 프로젝트/03.구현소스/JS/main.js
@@ -1,5 +1,18 @@
 //index JS영역
 
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
+
+// addEvent 함수
+// ele - 요소, evt - 이벤트, fn - 함수
+const addEvt = 
+(ele, evt, fn) => ele.addEventListener(evt, fn);
+
+// HTML태그 로딩후 loadFn함수 호출! ///
+addEvt(window,"DOMContentLoaded", loadFn);
+
+
+
 let aa = document.querySelectorAll('.sbili a');
 let bb = document.querySelector('.bslider ul');
 aa.forEach((ele,idx)=>{
@@ -18,67 +31,81 @@ const nlist = document.querySelector('.thumbbx ul')
 const nimg = document.querySelectorAll('.thumbbx img')
 //console.log('이미지 잡힘?',nimg);
 
-//뒤로가기 화살표
-const prevBtn = document.querySelector('#prevBtn')
+// //뒤로가기 화살표
+// const prevBtn = document.querySelector('#prevBtn')
 
-//다음이동 화살표
-const nextBtn = document.querySelector('#nextBtn')
-//console.log('이미지 잡힘?',nextBtn);
-
-/* 카운터 값(=클릭 시 이동하는 이미지 수?) 설정*/
-let counter = 1;
+// //다음이동 화살표
+// const nextBtn = document.querySelector('#nextBtn')
+// //console.log('이미지 잡힘?',nextBtn);
 
 
-/* 리스트 이미지로 가로값 지정 */
-const size = nimg[0].clientWidth;
-//애니메이션 초기값 설정인감..
-nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-//////이동버튼설정///////////////////////////////////////
+function loadFn(){
+  console.log('로딩완료');
 
-//머.. 넥스트 버튼을 클릭하면 이벤트를 추가 실행.. 
-nextBtn.addEventListener('click', ()=>{
-
-  //카운터 보다 크거나 같으면 이미지 갯수 -1 만큼 머..???
-  if(counter >= 5) return;
-
-  //리스트박스 애니메이션
-  nlist.style.transition = "transform .4s ease-in-out";
-  counter++;
-
-  nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-  //'translateX(' + (-size * counter) + 'px)'은 무엇..???
-});/////////////////////////nextBtn 이벤트 설정 ///////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-
-prevBtn.addEventListener('click',()=>{
-  console.log(counter);
-  if(counter <= 0) return;
-
-  //리스트박스 애니메이션
-  nlist.style.transition = "transform .4s ease-in-out";
+  //이동버튼 대상: .abtn
   
-  counter--;
-  nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});////////////////////////////이전이동 버튼 ////////////
 
-nlist.addEventListener('transitionend',() => {
+}//////////////////loadFn//////////
 
-console.log(nimg[counter],counter);
-if(nimg[counter].id === 'lastclone'){
-  nlist.style.transition = "none"; //트렌지션 효과 제거
-  counter = nimg.length -2; //counter 초기화
-  nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';//실제 이미지로 이동
+
+
+/* 슬라이드 비교파일_이전버전 */
+
+// /* 카운터 값(=클릭 시 이동하는 이미지 수?) 설정*/
+// let counter = 1;
+
+
+// /* 리스트 이미지로 가로값 지정 */
+// const size = nimg[0].clientWidth;
+// //애니메이션 초기값 설정인감..
+// nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// //////이동버튼설정///////////////////////////////////////
+
+// //머.. 넥스트 버튼을 클릭하면 이벤트를 추가 실행.. 
+// nextBtn.addEventListener('click', ()=>{
+
+//   //카운터 보다 크거나 같으면 이미지 갯수 -1 만큼 머..???
+//   if(counter >= 5) return;
+
+//   //리스트박스 애니메이션
+//   nlist.style.transition = "transform .4s ease-in-out";
+//   counter++;
+
+//   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//   //'translateX(' + (-size * counter) + 'px)'은 무엇..???
+// });/////////////////////////nextBtn 이벤트 설정 ///////////////////////////
+// /////////////////////////////////////////////////////////////////////////
+
+
+// prevBtn.addEventListener('click',()=>{
+//   console.log(counter);
+//   if(counter <= 0) return;
+
+//   //리스트박스 애니메이션
+//   nlist.style.transition = "transform .4s ease-in-out";
   
-}////////실제 이미지 마지막 이동
+//   counter--;
+//   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// });////////////////////////////이전이동 버튼 ////////////
 
-else if(nimg[counter].id === 'firstclone'){
-  nlist.style.transition = 'none';
-  counter = nimg.length -counter;// 카운터 값 초기화  
-  nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}///////실제 이미지 첫번째 이동
+// nlist.addEventListener('transitionend',() => {
 
-});//////////////////nlist 이벤트/////////////////////////////////////
+// console.log(nimg[counter],counter);
+// if(nimg[counter].id === 'lastclone'){
+//   nlist.style.transition = "none"; //트렌지션 효과 제거
+//   counter = nimg.length -2; //counter 초기화
+//   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';//실제 이미지로 이동
+  
+// }////////실제 이미지 마지막 이동
+
+// else if(nimg[counter].id === 'firstclone'){
+//   nlist.style.transition = 'none';
+//   counter = nimg.length -counter;// 카운터 값 초기화  
+//   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// }///////실제 이미지 첫번째 이동
+
+// });//////////////////nlist 이벤트/////////////////////////////////////
 
