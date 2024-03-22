b85268c61cb030908ad6733e7b3f0bbb9c569741
ParkSuMin0611 <71934605+ParkSuMin0611@users.noreply.github.com>
2024-03-22 18:18:11 +0900

    asd

--- 1차 프로젝트/03.구현소스/JS/main.js
+++ 1차 프로젝트/03.구현소스/JS/main.js
@@ -5,30 +5,26 @@ const qsa = (x) => document.querySelectorAll(x);
 
 // addEvent 함수
 // ele - 요소, evt - 이벤트, fn - 함수
-const addEvt = 
-(ele, evt, fn) => ele.addEventListener(evt, fn);
+const addEvt = (ele, evt, fn) => ele.addEventListener(evt, fn);
 
 // HTML태그 로딩후 loadFn함수 호출! ///
-addEvt(window,"DOMContentLoaded", loadFn);
+addEvt(window, "DOMContentLoaded", loadFn);
 
-
-
-let aa = document.querySelectorAll('.sbili a');
-let bb = document.querySelector('.bslider ul');
-aa.forEach((ele,idx)=>{
-  ele.onclick=(e)=>{
+let aa = document.querySelectorAll(".sbili a");
+let bb = document.querySelector(".bslider ul");
+aa.forEach((ele, idx) => {
+  ele.onclick = (e) => {
     e.preventDefault();
-    bb.style.left = (-100*idx)+"%";
-    bb.style.transition = '.6s';
-  }
+    bb.style.left = -100 * idx + "%";
+    bb.style.transition = ".6s";
+  };
 });
 
-
 /* 리스트 박스 */
-const nlist = document.querySelector('.thumbbx ul')
+
 //console.log('리스트 잡힘?',nlist);
 /* 리스트 내용물*/
-const nimg = document.querySelectorAll('.thumbbx img')
+const nimg = document.querySelectorAll(".thumbbx img");
 //console.log('이미지 잡힘?',nimg);
 
 // //뒤로가기 화살표
@@ -38,23 +34,70 @@ const nimg = document.querySelectorAll('.thumbbx img')
 // const nextBtn = document.querySelector('#nextBtn')
 // //console.log('이미지 잡힘?',nextBtn);
 
+function loadFn() {
+  console.log("로딩완료");
 
-
-function loadFn(){
-  console.log('로딩완료');
-
+  //1. 이동대상 설정
   //이동버튼 대상: .abtn
-  
+  // const abtn = qsa(".abtn");
+  const abtn = document.querySelectorAll(".abtn");
+  // document.querySelectorAll('abtn')
+  // console.log(document.querySelectorAll('.abtn'));
+  //변경대상: .thumbbx ul
+  const slide = qs(".thumbbx ul");
 
-}//////////////////loadFn//////////
+  //슬라이드 전역변수
+  let snum = 0;
 
+  //2. 버튼 이벤트 설정
+  for (let x of abtn) {
+    x.onclick = goslide;
+  }
 
+  function goslide() {
+    let isRbtn = this.classList.contains("ab2");
+    //contains: 특정 클래스 여부 판단, true / false
+    //함수 호출 확인
+    console.log("나 슬라이드야!", this, isRbtn);
+
+    if (isRbtn) {
+      slide.style.left = "-100%";
+      slide.style.transition = ".6s ease-in-out";
+
+      setTimeout(() => {
+        //2-1 맨앞 li 맨 뒤로 이동
+        slide.appendChild(slide.querySelectorAll("li")[0]);
+        //슬라이드 left값이 -100%이므로
+        //2-2 left값을 0으로 변경
+        slide.style.left = "0";
+        //2-3 left 트렌지션 없애기
+        slide.style.transition = "none";
+      }, 600); //////////timout///////
+    } /////////if
+  } ///////////goslide///////////
+
+  /*  if (isRbtn) {
+    // (1)먼저 왼쪽으로 이동하기
+    slide.style.left = "-100%";
+    slide.style.transition = ".6s ease-in-out";
+
+    // (2)이동하는 시간 0.6초간 기다림!
+    setTimeout(() => {
+      // (2-1) 맨앞 li 맨뒤로 이동
+      slide.appendChild(
+          slide.querySelectorAll("li")[0]);
+      // 슬라이드 left 값이 -100% 이므로
+      // (2-2) left값을 0으로 변경
+      slide.style.left = '0';
+      // (2-3) left 트랜지션 없애기
+      slide.style.transition = 'none';
+    }, 600); */
+} ////////////////// goslide//////////
 
 /* 슬라이드 비교파일_이전버전 */
 
 // /* 카운터 값(=클릭 시 이동하는 이미지 수?) 설정*/
 // let counter = 1;
-
 
 // /* 리스트 이미지로 가로값 지정 */
 // const size = nimg[0].clientWidth;
@@ -63,7 +106,7 @@ function loadFn(){
 
 // //////이동버튼설정///////////////////////////////////////
 
-// //머.. 넥스트 버튼을 클릭하면 이벤트를 추가 실행.. 
+// //머.. 넥스트 버튼을 클릭하면 이벤트를 추가 실행..
 // nextBtn.addEventListener('click', ()=>{
 
 //   //카운터 보다 크거나 같으면 이미지 갯수 -1 만큼 머..???
@@ -79,14 +122,13 @@ function loadFn(){
 // });/////////////////////////nextBtn 이벤트 설정 ///////////////////////////
 // /////////////////////////////////////////////////////////////////////////
 
-
 // prevBtn.addEventListener('click',()=>{
 //   console.log(counter);
 //   if(counter <= 0) return;
 
 //   //리스트박스 애니메이션
 //   nlist.style.transition = "transform .4s ease-in-out";
-  
+
 //   counter--;
 //   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
 // });////////////////////////////이전이동 버튼 ////////////
@@ -98,14 +140,13 @@ function loadFn(){
 //   nlist.style.transition = "none"; //트렌지션 효과 제거
 //   counter = nimg.length -2; //counter 초기화
 //   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';//실제 이미지로 이동
-  
+
 // }////////실제 이미지 마지막 이동
 
 // else if(nimg[counter].id === 'firstclone'){
 //   nlist.style.transition = 'none';
-//   counter = nimg.length -counter;// 카운터 값 초기화  
+//   counter = nimg.length -counter;// 카운터 값 초기화
 //   nlist.style.transform = 'translateX(' + (-size * counter) + 'px)';
 // }///////실제 이미지 첫번째 이동
 
 // });//////////////////nlist 이벤트/////////////////////////////////////
-
