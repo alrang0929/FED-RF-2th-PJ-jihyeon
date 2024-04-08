dd62919f1a4aa34ae4b4a680d9e1ee6ced514f4d
ParkSuMin0611 <71934605+ParkSuMin0611@users.noreply.github.com>
2024-04-08 15:23:38 +0900

    asd

---
+++ 1차 프로젝트/03.구현소스/data/slide_data.js
@@ -0,0 +1,24 @@
+const sdData = [
+
+{idx: 1,
+title: "Sustainability",
+text: "아모레퍼시픽은 자연과 사람, 사회가 조화롭게 공존하는 아름다운 세상을 만들어갑니다. 모든 구성원이 함께 더 나은 미래를 꿈꿉니다.",
+BG: "./images/sub01/sd01.jpg"
+},
+{idx: 2,
+title: "Sustainability",
+text: "아모레퍼시픽은 자연과 사람, 사회가 조화롭게 공존하는 아름다운 세상을 만들어갑니다. 모든 구성원이 함께 더 나은 미래를 꿈꿉니다.",
+BG: "./images/sub01/sd01.jpg"
+},
+{idx: 3,
+title: "Sustainability",
+text: "아모레퍼시픽은 자연과 사람, 사회가 조화롭게 공존하는 아름다운 세상을 만들어갑니다. 모든 구성원이 함께 더 나은 미래를 꿈꿉니다.",
+BG: "./images/sub01/sd01.jpg"
+},
+{idx: 4,
+title: "Sustainability",
+text: "아모레퍼시픽은 자연과 사람, 사회가 조화롭게 공존하는 아름다운 세상을 만들어갑니다. 모든 구성원이 함께 더 나은 미래를 꿈꿉니다.",
+BG: "./images/sub01/sd01.jpg"
+},
+
+]////sdData배열
