484b1c196d51d7aee02f582af4b103296e485ec0
ParkSuMin0611 <sellbell07@gmail.com>
2024-04-29 18:16:34 +0900

    asd

--- 팀 프로젝트/JS/sub_badbatch.js
+++ 팀 프로젝트/JS/sub_badbatch.js
@@ -2,5 +2,8 @@
 import mFn from "./my_function.js"
 import * as listMap from "./sub_mapping.js"
 
-
 ///////불러오기 영역//////////////////////
+
+/* 슬라이드 리스트 맵핑 */
+listMap.epItems();
+listMap.snsItems();
--- 팀 프로젝트/JS/sub_mapping.js
+++ 팀 프로젝트/JS/sub_mapping.js
@@ -4,26 +4,74 @@ import mFn from "./my_function.js"
 import * as listData from "../data/sub_list_data.js"
 ///////불러오기 영역//////////////////////
 
-//1. 대상선정
-const epSlide = mFn.qs('.epSlide')
-// console.log(epArea);
-epSlide.innerHTML = `
-<ul class="fx-box col-8">
-    ${listData.epData.map(v =>`
-    
-        <li>
-            <figure>
-            <img src="./sub_badbatch/img/thumb_.jpg" alt="Episode Guides | New Each 
-              Thursday @ 9AM PT" /></figure>
-            <figcaption>
-              <div class="title-topic">Season3, Episode1</div>
-              <h4 class="title-sub2">
-                Episode Guides | New Each Thursday @ 9AM P
-              </h4>
-            </figcaption>
-        </li>
-    
-    `).join('')}
 
-</ul>
-`
+//에피소드 리스트 li
+function epItems(){
+  //1. 대상선정
+  const epSlide = mFn.qs('.epSlide')
+  // console.log(epArea);
+  epSlide.innerHTML = `
+  <ul class="fx-box col-8">
+  ${listData.epData.map(v =>`
+  <li>
+  <figure>
+  <img src="./sub_badbatch/img/thumb_${v.idx}.jpg" alt="E${v.title}" /></figure>
+  <figcaption>
+  <div class="title-topic">Season3, Episode${v.idx}</div>
+  <h4 class="title-sub2">
+  ${v.title}
+  </h4>
+  </figcaption>
+  </li>
+  `).join('')}
+  </ul>
+  `
+}//////epItem//////////////////////////////
+
+function snsItems(){
+
+  mFn.qs('.snsSlide').innerHTML = `
+  <ul class="fx-box col-8">
+  ${listData.snsData.map(v=>`
+    <li>
+      <figure><a href="${v.link}" target="_blank">
+        <img src="./sub_badbatch/img/poster_${v.idx}.jpg" alt="${v.title}" />
+      </a></figure>
+      <figcaption>
+        <div class="title-topic">Season3, Episode${v.idx}</div>
+        <h4 class="title-sub2">
+        ${v.title}
+        </h4>
+      </figcaption>
+    </li>
+    `).join('')}
+    </ul>
+  `
+}
+
+const posItems = mFn.qs(".pos-wrap")
+
+let hcode = `<ul class="slider-wrap gr-box">`;
+for(let i = 1 ; i <= 15 ; i++){
+ hcode += `
+  <li>
+  <a href="#">
+  <img src="./sub_badbatch/img/poster_${i}.jpg" 
+  alt="스타워즈 포스터${i}"></a>
+  </li>
+  `;
+}////for
+hcode += '</ul>'
+posItems.innerHTML = hcode;
+
+
+
+
+
+
+
+
+///////내보내기 영역//////////////////////
+export {epItems,snsItems, }
+
+
