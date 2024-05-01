590076543dde0b16f95734b7a3c4b4c6e3ce9a68
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-01 11:23:29 +0900

    asd

--- 팀 프로젝트/JS/sub_badbatch.js
+++ 팀 프로젝트/JS/sub_badbatch.js
@@ -1,7 +1,7 @@
 //메인sub badbatch js - sub_badbatch.js
 import mFn from "./my_function.js"
 import * as listMap from "./sub_mapping.js"
-import * as slideFn from "./slide.js";
+import setSlide from "./slide.js";
 ///////불러오기 영역//////////////////////
 
 // console.log(slideFn.carouselSlider);
@@ -11,4 +11,4 @@ listMap.epItems();
 listMap.snsItems();
 
 //슬라이드 함수 호출
-slideFn.carouselSlider();
+setSlide('banbx');
