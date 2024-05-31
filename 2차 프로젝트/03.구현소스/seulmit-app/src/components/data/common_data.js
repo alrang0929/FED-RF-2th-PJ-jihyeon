1a2d0ebd51295137e7a5b92d34d50b4d522358b8
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-31 11:23:09 +0900

    asd

---
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/data/common_data.js
@@ -0,0 +1,38 @@
+///메뉴 카테고리 데이터
+
+const gnbData = {
+    "SHOP":{
+        "submenu":["All", "Face", "Body", "Hand", "Home Fragrance", "Gift", "Acc",],
+    },
+    "INSIDE":{
+        "submenu":[
+            "Brand",
+            "Episode",
+        ],
+    },
+    "TASTE":{
+        "submenu":[
+            "none",
+        ],
+    },
+    "BENEFIT":{
+        "submenu":[
+            "BENEFIT",
+            "BENEFIT",
+        ],
+    },
+    "LOGIN":{
+        "submenu":[
+            "none",
+        ],
+    },
+    "NOTICE":{
+        "submenu":[
+            "NOTICE",
+            "NOTICE",
+            "NOTICE",
+        ],
+    },
+
+}
+
