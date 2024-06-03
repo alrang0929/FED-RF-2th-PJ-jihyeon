2c1b4e11c8c6e825bec26c19a55ef7c1619d2e78
alrang0929 <sellbell07@gmail.com>
2024-06-04 08:24:17 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/index.js
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/index.js
@@ -1,7 +1,22 @@
 //index js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 //sass 패키지 호출
 import "./css/main.scss";
-
+//컴포넌트 호출
+// import "./components/layout/Intro";
+import "./components/layout/TopArea";
+import "./components/layout/MainArea";
+import "./components/layout/FooterArea";
 ///////import area///////////////////////////////////
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root'));
+    root.render(
+        <>
+            <h1> 왜안대</h1>
+        <TopArea />
+        <MainArea />
+        <FooterArea />
+        </>
+    );
