25d7b6171677fcd70440e0ce1d0ac4adaaae95f4
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-04 10:22:04 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/index.js
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/index.js
@@ -1,22 +1,24 @@
 //index js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 //sass 패키지 호출
 import "./css/main.scss";
 //컴포넌트 호출
 // import "./components/layout/Intro";
-import "./components/layout/TopArea";
-import "./components/layout/MainArea";
-import "./components/layout/FooterArea";
+import TopArea from "./components/layout/TopArea";
+import MainArea from "./components/layout/MainArea";
+import FooterArea from "./components/layout/FooterArea";
 ///////import area///////////////////////////////////
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root'));
-    root.render(
-        <>
-            <h1> 왜안대</h1>
-        <TopArea />
-        <MainArea />
-        <FooterArea />
-        </>
-    );
+function Layout() {
+  return (
+    <>
+      <h1> 왜안대</h1>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </>
+  );/////return
+} //////layout
+
+ReactDOM.render(<Layout />, document.querySelector("#root"));
