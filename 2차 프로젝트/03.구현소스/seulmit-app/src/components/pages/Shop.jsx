3ece8fe1a4e61afd2a6dc8a53beed2f60db999f9
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-07 17:54:39 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Shop.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Shop.jsx
@@ -8,81 +8,103 @@ import { gnbData } from "../data/gnb_data";
 import "../../css/sub_sellpage.scss";
 /////import area/////////////////////////////////////////////////
 
-export default function Shop(){
-
-    //// 코드 리턴구역 //////////////
-    return(
-        <>
-         <div id="sub-header">
-            <div className="title">
-                <div className="icon">
-                    <img src="/images/icon-sim-black.svg" alt="" />
-                </div>
-                <h3>NOTICE</h3>
-                <span>슬밋의 공지사항을 확인하세요</span>
-            </div>
+export default function Shop() {
+  //// 코드 리턴구역 //////////////
+  return (
+    <>
+      <div className={`sub-header bg1 cbx bgi`}>
+        <div className="title">
+          <div className="icon">
+            <img src="/images/icon-sim-black.svg" alt="" />
+          </div>
+          <h3>NOTICE</h3>
+          <span>슬밋의 공지사항을 확인하세요</span>
         </div>
+      </div>
 
-        <section id="pd-list-set">
+      <section id="sell-pd-list" className="cont-box">
         {/* 상단 메뉴 리스트 */}
         <div className="category">
-            <ul>
-                <li className="select-ef"><a href="">All</a></li>
-                <li className=""><a href="">FACE</a></li>
-                <li className=""><a href="">FACE</a></li>
-                <li className=""><a href="">FACE</a></li>
-                <li className=""><a href="">FACE</a></li>
-                <li className=""><a href="">FACE</a></li>
-                <li className=""><a href="">FACE</a></li>
-            </ul>
-            <span className="under-line"></span>
+          <ul className="fx-box">
+            <li className="">
+              <a className="on" href="">
+                All
+              </a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+            <li className="">
+              <a href="">FACE</a>
+            </li>
+          </ul>
+          <div className="under-line"></div>
         </div>
         {/* 메뉴 리스트 */}
         <div className="pd-list">
-        <ul className="slider fx-box cont-box">
-          {bestItemData.map((v, i) => (
-            <li key={i} className="item">
-              <a href="">
-                <div className="desc-wrap">
-                  <div className="eng-pdtit">{v.engtit}</div>
-                  <div className="kor-pdtit">{v.tit}</div>
-                  <div className="price">{v.price}</div>
-                </div>
-
-                <div className="img-wrap">
+          <ul className="fx-box">
+            {bestItemData.map((v, i) => (
+              <li key={i} className="item">
+                <a href="">
                   <div className="imgbx">
                     <img
                       src={`./images/product/${v.category}/${v.img}.jpg`}
                       alt={`"${v.tit}"`}
                     />
                   </div>
-                </div>
-              </a>
-            </li>
-          ))}
-        </ul>
+                  <div className="desc-wrap">
+                    <div className="eng-pdtit">{v.engtit}</div>
+                    <div className="kor-pdtit">{v.tit}</div>
+                    <div className="price">{v.price} <span>원</span></div>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
         {/* page Num */}
         <div className="page-num fx-box">
-            <div className="prev">
-                <span className="icon-img material-symbols-outlined">chevron_left</span>
-            </div>
-            <ul>
-                <li className="select-ef"><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
-                <li><a href="">4</a></li>
-                <li><a href="">5</a></li>
-            </ul>
-            <div className="next">
-                <span className="icon-img material-symbols-outlined">chevron_right</span>
-            </div>
+          <div className="prev">
+            <span className="icon-img material-symbols-outlined">
+              chevron_left
+            </span>
+          </div>
+          <ul className="fx-box">
+            <li className="select-ef">
+              <a href="">1</a>
+            </li>
+            <li>
+              <a href="">2</a>
+            </li>
+            <li>
+              <a href="">3</a>
+            </li>
+            <li>
+              <a href="">4</a>
+            </li>
+            <li>
+              <a href="">5</a>
+            </li>
+          </ul>
+          <div className="next">
+            <span className="icon-img material-symbols-outlined">
+              chevron_right
+            </span>
+          </div>
         </div>
-        </section>
-
-
-
-        </>
-    );
-
+      </section>
+    </>
+  );
 } /////////// Shop /////////////////////
