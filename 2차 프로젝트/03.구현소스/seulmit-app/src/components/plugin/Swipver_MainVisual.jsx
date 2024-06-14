d371006bb8bbaac366f0f74051a30acfd2ed35e5
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-14 15:37:19 +0900

    afds

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/plugin/Swipver_MainVisual.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/plugin/Swipver_MainVisual.jsx
@@ -10,7 +10,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "./css/swipver_MainVisual.scss";
-import { Pagination, Autoplay, Virtual } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 //////////////////////import area/////////////////////////////
 
 export default function SwipverMainVisual() {
@@ -22,32 +22,18 @@ export default function SwipverMainVisual() {
 
     $descWrap
       .css({
-        filter: "blur(50px)",
-        display: "block",
         bottom: "70%",
-        blur: "200px",
         opacity: "0",
       })
       .animate(
         {
-          filter: "blur(0px)",
           bottom: "20%",
           opacity: "1",
         },
         2000
       )
       .delay(2000)
-      .animate(
-        {
-          filter: "blur(0px)",
-          bottom: "20%",
-          opacity: "0",
-        },
-        1000,
-        () => {
-          $descWrap.stop(true, true);
-        }
-      );
+      .fadeTo(1000, 0);
   };
 
   //////코드리턴구역
@@ -60,54 +46,17 @@ export default function SwipverMainVisual() {
         pagination={{
           clickable: true,
         }}
-        // autoplay={
-        //     // 자동 슬라이드 설정 , 비 활성화 시 false
-        //     { delay: 5500, disableOnInteraction: false }
-        // }
-        observer={true}
+        autoplay={
+          // 자동 슬라이드 설정 , 비 활성화 시 false
+          { delay: 5500, disableOnInteraction: false }
+        }
         onInit={(swiper) => {
           // Swiper 초기화 완료 후 실행되는 코드
           descAni(swiper); // 초기 슬라이드 애니메이션 실행
         }}
-        onSlideChangeTransitionStart={(swiper) => {
-          const realIndex = swiper.realIndex;
-          const $activeSlide = $(swiper.slides[realIndex]);
-          const $descWrap = $activeSlide.find(".desc-wrap");
-          const $bgbox = $activeSlide.find(".item");
-
-          console.log("배경박스", $bgbox);
-
-          $descWrap
-            .css({
-              filter: "blur(50px)",
-              display: "block", // 슬라이드 변경 직전에 display: block 설정
-              bottom: "70%",
-              opacity: "0",
-            })
-            .animate(
-              {
-                filter: "blur(0px)",
-                bottom: "20%",
-                opacity: "1",
-              },
-              2000
-            )
-            .delay(2000)
-            .animate(
-              {
-                filter: "blur(0px)",
-                bottom: "20%",
-                opacity: "1",
-              },
-              1000,
-              () => {
-                $descWrap.stop(true, true);
-              }
-            );
+        onSlideChangeTransitionEnd={(swiper) => {
+          descAni(swiper);
         }}
-        // onSlideChangeTransitionEnd={(swiper) => {
-        //   descAni(swiper);
-        // }}
         speed={20e2}
         modules={[Pagination, Autoplay]}
         className="mySwiper main-visual-inner"
@@ -117,7 +66,7 @@ export default function SwipverMainVisual() {
             key={i}
             className={`"item main-box bg${i + 1} cbx bgi swiper-slide"`}
           >
-            <div className="desc-wrap" style={{ opacity: "0" }}>
+            <div className="desc-wrap">
               <div className="topic">{v.topic}</div>
               <div className="title">{v.title}</div>
               <div className="text">{v.text}</div>
