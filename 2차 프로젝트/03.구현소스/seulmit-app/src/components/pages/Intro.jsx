57b74bc9c50aeef8b8abe721a765edc5d32aeac8
alrang0929 <sellbell07@gmail.com>
2024-06-16 23:20:29 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Intro.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Intro.jsx
@@ -1,43 +1,74 @@
-import "../../css/intro.scss";
-import introFn from "../func/intro";
-import React, { useContext } from "react";
-import { sCon } from "./sCon";
+//슬밋pj - 인트로페이지 jsx
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import $ from "jquery";
+
+////module
+import { sCon } from "./sCon";
+import introFn from "../func/intro";
+
+import "../../css/intro.scss";
+//////////////////import area///////////////////////
 
 export default function Intro() {
-  React.useEffect(() => {
-    // introFn();
+    const bar = $(".loading");
+    console.log("로딩바", bar);
+    const [IsLoading, setIsLoading] = useState(true);
 
-    return(()=>{
-      myCon.setIntroSts(false);
-    });
-  },[]);
+    React.useEffect(() => {
+      const checkResourcesLoaded = async () => {
+        // 이미지, 비디오 등 외부 리소스 로딩 확인 로직
+        await Promise.all([
+          new Promise((resolve) => {
+            const img = new Image();
+            img.src = "/images/icon-sim-white.svg";
+            img.onload = resolve;
+          }),
+          // ... (다른 리소스 로딩 확인)
+        ]);
+    
+        setIsLoading(false); // 모든 리소스 로딩 완료 후 state 변경
+      };
+    
+      checkResourcesLoaded();
+    }, []);
+    let myCon = useContext(sCon);
 
-  let myCon = useContext(sCon);
-
-  ////// 코드 리턴구역 //////////////
-  return (
-    <>
-      <div id="intro-area">
-        <Link to="#" onClick={(e)=>{e.preventDefault();myCon.setIntroSts(false);}}>
-          <div className="cont-wrap">
-            <span>
-              <img src="/images/icon-sim-white.svg" alt="" />
-            </span>
-            <img src="/images/logo_ko.svg" alt="" />
-            <div className="desc">
-              로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은 출판이나 그래픽
-              디자인 분야에서 폰트, 타이포그래피, 레이아웃 같은
+    ////// 코드 리턴구역 //////////////
+    return (
+        <>
+            <div id="intro-area">
+                {!IsLoading && (
+                    <Link
+                        to="/"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            myCon.setIntroSts(false);
+                        }}
+                    >
+                        <div className="cont-wrap">
+                            <span>
+                                <img src="/images/icon-sim-white.svg" alt="" />
+                            </span>
+                            <img src="/images/logo_ko.svg" alt="" />
+                            <div className="desc">
+                                로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은
+                                출판이나 그래픽 디자인 분야에서 폰트,
+                                타이포그래피, 레이아웃 같은
+                            </div>
+                            <div className="loading"></div>
+                        </div>
+                        <div className="bg">
+                            <video id="myvid" muted autoPlay loop>
+                                <source
+                                    src="/images/intro_bg1.mp4"
+                                    type="video/mp4"
+                                />
+                            </video>
+                        </div>
+                    </Link>
+                )}
             </div>
-            <div className="loading"></div>
-          </div>
-          <div className="bg">
-            <video id="myvid" muted autoPlay loop>
-              <source src="/images/intro_bg1.mp4" type="video/mp4" />
-            </video>
-          </div>
-        </Link>
-      </div>
-    </>
-  ); ///////return
+        </>
+    ); ///////return
 }
--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/plugin/Swipver_MainVisual.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/plugin/Swipver_MainVisual.jsx
@@ -14,81 +14,76 @@ import { Pagination, Autoplay } from "swiper/modules";
 //////////////////////import area/////////////////////////////
 
 export default function SwipverMainVisual() {
-  //각 배너의 left 값이 0이 되었을때 .desc-wrap가 페이드인 + 올라오기
-  const descAni = (swiper) => {
-      const activeIndex = swiper.activeIndex; // activeIndex 사용
-      const $activeSlide = $(swiper.slides[activeIndex]);
-      const $descWrap = $activeSlide.find(".desc-wrap");
-  
+    //각 배너의 left 값이 0이 되었을때 .desc-wrap가 페이드인 + 올라오기
+    const descAni = (swiper) => {
+        const activeIndex = swiper.activeIndex; // activeIndex 사용
+        const $activeSlide = $(swiper.slides[activeIndex]);
+        const $descWrap = $activeSlide.find(".desc-wrap");
 
-//제이쿼리 animate는 fiter값을 전달 못받음.
-// 따라서 작업시 step (< 제이쿼리 중간에 ani 추가해줌) 을 추가하여 진행할 것
-//아래 같은 경우는 fx.prop(animate값) 중 opacity 를 if 값으로 받아와 실행시킴
+        //제이쿼리 animate는 fiter값을 전달 못받음.
+        // 따라서 작업시 step (< 제이쿼리 중간에 ani 추가해줌) 을 추가하여 진행할 것
+        //아래 같은 경우는 fx.prop(animate값) 중 opacity 를 if 값으로 받아와 실행시킴
 
-      $descWrap.eq(0)
-      .css({
-        bottom: "70%",
-        opacity: "0",
-      })
-      .animate(
-        {
-          bottom: "20%",
-          opacity: "1",
-        },
-        {
-          duration: 3000,
-          step: function(now, fx) { // step 콜백 함수 추가
-            if (fx.prop === 'opacity') {
-              $(this).css('filter', `blur(${50 * (1 - now)}px)`); // 블러 효과 계산
-            }
-          },
-        }
-      )
-      .delay(2000)
-      .fadeTo(1000, 0);
-      }
+        $descWrap
+            .eq(0)
+            .css({
+                bottom: "70%",
+                opacity: "0",
+            })
+            .animate(
+                {
+                    bottom: "20%",
+                    opacity: "1",
+                },
+                {
+                    duration: 3000,
+                    step: function (now, fx) {
+                        // step 콜백 함수 추가
+                        if (fx.prop === "opacity") {
+                            $(this).css("filter", `blur(${50 * (1 - now)}px)`); // 블러 효과 계산
+                        }
+                    },
+                }
+            )
+            .delay(2000)
+            .fadeTo(1000, 0);
+    };
 
-
-  //////코드리턴구역
-  return (
-    <>
-
-      <Swiper
-        loop={true}
-        loopedSlides={mainVisualData.length} // 복제 슬라이드 개수 설정
-        slidesPerView={1}
-        spaceBetween={0}
-        pagination={{
-          clickable: true,
-        }}
-        autoplay={
-          // 자동 슬라이드 설정 , 비 활성화 시 false
-          { delay: 5500, disableOnInteraction: false }
-        }
-        onInit={(swiper) => {
-          // Swiper 초기화 완료 후 실행되는 코드
-          descAni(swiper); // 초기 슬라이드 애니메이션 실행
-        }}
-        onSlideChangeTransitionEnd={(swiper) => {
-          descAni(swiper);
-        }}
-        speed={20e2}
-        modules={[Pagination, Autoplay]}
-        className="mySwiper main-visual-inner"
-      >
-        {mainVisualData.map((v, i) => (
-          <SwiperSlide
-            key={i}
-            className={`"item main-box bg${i + 1} cbx bgi swiper-slide"`}
-          >
-            <div className="desc-wrap">
-              <div className="topic">{v.topic}</div>
-              <div className="title">{v.title}</div>
-              <div className="text">{v.text}</div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </> 
-  );
+    //////코드리턴구역
+    return (
+        <>
+            <Swiper
+                loop={true}
+                loopedslides={mainVisualData.length} // 복제 슬라이드 개수 설정
+                slidesPerView={1}
+                spaceBetween={0}
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 5500, disableOnInteraction: false }}
+                onInit={(swiper) => {
+                    descAni(swiper);
+                }}
+                onSlideChangeTransitionEnd={(swiper) => {
+                    descAni(swiper);
+                }}
+                speed={20e2}
+                modules={[Pagination, Autoplay]} // Virtual 모듈 제거
+                className="mySwiper main-visual-inner"
+            >
+                {mainVisualData.map((v, i) => (
+                    <SwiperSlide
+                        key={i}
+                        className={`"item main-box bg${
+                            i + 1
+                        } cbx bgi swiper-slide"`}
+                    >
+                        <div className="desc-wrap">
+                            <div className="topic">{v.topic}</div>
+                            <div className="title">{v.title}</div>
+                            <div className="text">{v.text}</div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
+    );
 }
