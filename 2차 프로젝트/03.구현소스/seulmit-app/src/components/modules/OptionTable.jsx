c910717a201d617d650189444ce8fb30f7d0dc94
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-18 14:35:35 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/modules/OptionTable.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/modules/OptionTable.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React from "react";
 
 //data
 import { options, product } from "../data/product";
 /////import area////////////////////////////
 
-function OptionTable({catName}) {
+function OptionTable({ catName }) {
+  const selData = product.face[0];
 
-    const selData = product.face[0];
-
-    //코드리턴구역/////////////////////////////////////////////
-    return (
-        <>
-            <tbody className='default-option'>
-                <tr>
-                    <td className='tit'>{selData.tit}</td>
-                    <td className='quantity'>
-                        <input 
-                        id="quantity"
-                        name='quantity_otp[]'
-                        type='number'
-                        />
-                        </td>
-                    <td className='price'>{selData.price}</td>
-                </tr>
-            </tbody>
-        </>
-    );
+  //코드리턴구역/////////////////////////////////////////////
+  return (
+    <>
+      <table>
+        <thead style={{display:"none"}}>
+            <tr>
+                <th scope="col">상품명</th>
+                <th scope="col">상품수</th>
+                <th scope="col">가격</th>
+            </tr>
+        </thead>
+        <tbody className="default-option">
+          <tr>
+            <td className="tit">{selData.tit}</td>
+            <td className="quantity">
+              <input id="quantity" name="quantity_otp[]" type="number" />
+            </td>
+            <td className="price">{selData.price}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default OptionTable;
