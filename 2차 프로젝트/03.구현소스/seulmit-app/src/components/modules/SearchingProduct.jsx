c929fefcf62053007074a826a7fe7f614df8c304
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-26 15:39:02 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/modules/SearchingProduct.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/modules/SearchingProduct.jsx
@@ -9,7 +9,7 @@ import SwiperPrdList from "../plugin/Swiper_prdList";
 ///import area//////////////////////////////
 
 function SearchingProduct({ dt }) {
-  console.log("dt",dt);
+  console.log("dt", dt);
   // console.log("dt img",dt.img[0]);
   //dt : 검색배열 데이터
   //total = 검색된 배열데이터 개수
@@ -19,63 +19,57 @@ function SearchingProduct({ dt }) {
   //dt: Searching에서 결과값 전달받음
   return (
     <>
-
-    
-      {/* case1. 데이터 개수 有 */}
-      {total > 0 && (
-        <ul className="plist">
-          {dt.map((v, i) => (
-            <li key={i}>
-              <Link
-                to="/detail"
-                state={{
-                  engtit: v.engtit, // 상품 영문 타이틀
-                  tit: v.tit, // 한글 타이틀
-                  txt: v.txt, // 상세내용
-                }}
-              >
-                <div className="desc-wrap">
-                  <div className="eng-pdtit">{v.engtit}</div>
-                  <div className="kor-pdtit">{v.tit}</div>
-                  <div className="desc">{v.txt}</div>
-                </div>
-                <div className="img-wrap">
-                  <div className="imgbx">
-                    {/* 비상.....................배열 머지 시켜서 key값이 없음 */}
-                    <img
-                      src={`${v.img[0].isrc}`}
-                      alt={`"${v.tit}"`}
-                    />
+      <section className="plist-wrap">
+        {/* case1. 데이터 개수 有 */}
+        {total > 0 && (
+          <ul className="plist">
+            {dt.map((v, i) => (
+              <li key={i}>
+                <Link
+                  to="/detail"
+                  state={{
+                    engtit: v.engtit, // 상품 영문 타이틀
+                    tit: v.tit, // 한글 타이틀
+                    txt: v.txt, // 상세내용
+                  }}
+                >
+                  <div className="desc-wrap">
+                    <div className="eng-pdtit">{v.engtit}</div>
+                    <div className="kor-pdtit">{v.tit}</div>
+                    <div className="desc">{v.txt}</div>
                   </div>
-                  <div className="blur">
-                    <img
-                      src={`${v.img[1].isrc}`}
-                      alt={`"${v.tit}"`}
-                    />
+                  <div className="img-wrap">
+                    <div className="imgbx">
+                      {/* 비상.....................배열 머지 시켜서 key값이 없음 */}
+                      <img src={`${v.img[0].isrc}`} alt={`"${v.tit}"`} />
+                    </div>
+                    <div className="blur">
+                      <img src={`${v.img[1].isrc}`} alt={`"${v.tit}"`} />
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
 
-      {/* case2. 데이터 없는 경우 */}
-      {total == 0 && (
-        <>
-          <h2>검색 결과가 존재하지 않습니다.</h2>
-          <section id="prd-list">
-            <div className="desc-box">
-              <div className="tit">lorem ipsum</div>
-              <span className="desc">
-                로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은 출판이나
-              </span>
-            </div>
-            {/* 베스트상품 리스트 swiper : Swiper_prd-list */}
-            <SwiperPrdList />
-          </section>
-        </>
-      )}
+        {/* case2. 데이터 없는 경우 */}
+        {total == 0 && (
+          <>
+            <h2>검색 결과가 존재하지 않습니다.</h2>
+            <section id="prd-list">
+              <div className="desc-box">
+                <div className="tit">lorem ipsum</div>
+                <span className="desc">
+                  로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은 출판이나
+                </span>
+              </div>
+              {/* 베스트상품 리스트 swiper : Swiper_prd-list */}
+              <SwiperPrdList />
+            </section>
+          </>
+        )}
+      </section>
     </>
   );
 }
--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/SearchPage.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/SearchPage.jsx
@@ -4,6 +4,11 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import Searching from "../modules/Searching";
 
+//css
+import "../../css/search.scss";
+
+//////import area /////////////////////////////////
+
 ///import area/////////////////////////////////////
 
 function SearchPage() {
@@ -12,15 +17,17 @@ function SearchPage() {
 
   //2. 넘겨온 키워드 받기
   const keyword = loc.state.keyword;
-  
+
   console.log("검색어:", keyword);
 
   //코드리턴구역////////////////////////////
   return (
-  <>
-  <h2>블라블라블라</h2>
-  <Searching kword={keyword}/>
-  </>
+    <>
+      <h2 className="title">블라블라블라</h2>
+      <section className="cont-box">
+        <Searching kword={keyword} />
+      </section>
+    </>
   );
 }
 
