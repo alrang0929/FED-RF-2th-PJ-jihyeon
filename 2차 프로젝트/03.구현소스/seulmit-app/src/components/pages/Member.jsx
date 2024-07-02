61614be1132251b5be7ece1a83be82e68716514b
ParkSuMin0611 <sellbell07@gmail.com>
2024-07-02 18:15:52 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Member.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Member.jsx
@@ -6,6 +6,7 @@ import "../../css/member.scss";
 //data
 import { terms } from "../data/terms_text";
 import { Link } from "react-router-dom";
+import { initData } from "../func/memFn";
 
 ////import area//////////////////////////////////
 
@@ -43,28 +44,73 @@ function Member(props) {
   const msgId = [
     "16자 이내, 영문,숫자만 입력하세요",
     "중복된 아이디 입니다. 다른 아이디를 입력하세요",
-  ]
+    "사용가능한 아이디 입니다",
+  ];
   //비밀번호
   const msgPw = [
     "영문,숫자,특수문자 포함 16자 이내",
     "필수 입력 입니다",
     "비밀번호가 일치하지 않습니다",
-  ]
+  ];
 
-//[3] 에러메세지 상태변수: 초기값 msgId[0]
-const [idMsg, setIdMsg] = useState(msgId[0]);
-const [pwMsg, setpwMsg] = useState(msgPw[0]);
+  //[3] 에러메세지 상태변수: 초기값 msgId[0]
+  const [idMsg, setIdMsg] = useState(msgId[0]);
+  const [pwMsg, setpwMsg] = useState(msgPw[0]);
 
-//[ 유효성 검사 함수 ]////////////////////////
-//1. 아이디 유효성 검사
-const changeUserId = (e)=>{
-  //0. 입력된 값 읽기
-  let val = e.target.value;
+  //[ 유효성 검사 함수 ]////////////////////////
+  //1. 아이디 유효성 검사
+  const changeUserId = (e) => {
+    //0. 입력된 값 읽기
+    let val = e.target.value;
 
-  
+    //1. 아이디 유효성 검사
+    const valid = /^[A-Za-z0-9+]{5,16}$/;
+    //유효성 검사볍: 정규식.test(값)
 
-};
+    //2. 입력값 확인
+    console.log(val);
+    //입력값 유효성 검사 ㄱ
+    if (valid.test(val)) {
+      console.log("통과");
+      //아이디 검사를 위해 기본 데이터 호출
+      initData();
+      //로컬스토리지에 "mem-data"가 없으면 초기셋팅
 
+      //중복 아이디 검사 start
+      //1. 로컬스 변수할당
+      let memData = localStorage.getItem("mem-data");
+      console.log(memData);
+
+      //2. 로컬스 객체변환(< 문자형이기 때문에)
+      memData = JSON.parse(memData);
+      console.log(memData);
+
+      // ㄴ> 배열로 변환 => 배열이니까 입력데이터가 기존 배열값에 있는지 검사(true, false로 출력)
+      let isT = memData.some((v) => v.uid === val);
+      console.log("중복ID 있음?", isT);
+
+      //4. true => 중복데이터 메세지 표시
+      if (isT) {
+        //에러메세지 업데이트
+        setIdMsg(msgId[1]);
+        //에러상태값 업데이트
+        setUserIdError(true);
+      } ///if////////////
+      //4-1. false => [성공 메세지] 표시
+      else {
+        setUserIdError(false);
+      } ///////else////////////////////
+    } ///if
+    //검사결과 에러일 때
+    else {
+      console.log("에러");
+      //아이디 에러상태 없데이트
+      setUserIdError(true);
+    } ///else
+
+    //실제 화면출력을 위해 setUserId에 상태 업데이트
+    setUserId(val);
+  };
 
   ////코드 리턴구역/////////////////////////
   return (
@@ -81,7 +127,39 @@ const changeUserId = (e)=>{
                   type="text"
                   maxLength="16"
                   placeholder="영문,숫자 포함 16자 이내"
+                  value={userId}
+                  onChange={changeUserId}
                 />
+                {
+                  // 에러 메세지 출력
+                  userIdError && (
+                    <div className="msg" style={{ paddingTop: "4px" }}>
+                      <small
+                        style={{
+                          color: "red",
+                          fontSize: "13px",
+                        }}
+                      >
+                        {idMsg}
+                      </small>
+                    </div>
+                  )
+                }
+                {
+                  // 통과 메세지
+                  !userIdError && userId && (
+                    <div className="msg" style={{ paddingTop: "4px" }}>
+                      <small
+                        style={{
+                          color: "blue",
+                          fontSize: "13px",
+                        }}
+                      >
+                        {msgId[2]}
+                      </small>
+                    </div>
+                  )
+                }
               </li>
               {/* 2. 비밀번호 */}
               <li>
@@ -90,7 +168,24 @@ const changeUserId = (e)=>{
                   type="password"
                   maxLength="16"
                   placeholder="영문,숫자,특수문자 포함 16자 이내"
+                  // value={pwd}
+                  // onChange={changePwd}
                 />
+                 {
+                  // 에러 메세지 출력
+                  pwdError && (
+                    <div className="msg" style={{ paddingTop: "4px" }}>
+                      <small
+                        style={{
+                          color: "red",
+                          fontSize: "13px",
+                        }}
+                      >
+                        {pwMsg}
+                      </small>
+                    </div>
+                  )
+                }
               </li>
               {/* 3. 비밀번호 확인 */}
               <li>
@@ -99,7 +194,24 @@ const changeUserId = (e)=>{
                   type="password"
                   maxLength="16"
                   placeholder="영문,숫자,특수문자 포함 16자 이내"
+                  // value={chkPwd}
+                  // onChange={changeChkPwd}
                 />
+                {
+                  // 에러 메세지 출력
+                  chkPwdError && (
+                    <div className="msg" style={{ paddingTop: "4px" }}>
+                      <small
+                        style={{
+                          color: "red",
+                          fontSize: "13px",
+                        }}
+                      >
+                        {msgPw[3]}
+                      </small>
+                    </div>
+                  )
+                }
               </li>
               {/* 4. 사용자 이름 */}
               <li>
@@ -108,6 +220,8 @@ const changeUserId = (e)=>{
                   type="text"
                   maxLength="16"
                   placeholder="실명으로 작성해주세요"
+                  // value={userName}
+                  // onChange={changeName}
                 />
               </li>
 
@@ -118,6 +232,8 @@ const changeUserId = (e)=>{
                   type="text"
                   maxLength="50"
                   placeholder="이메일을 입력해주세요"
+                  // value={email}
+                  // onChange={changeEmail}
                 />
               </li>
             </ul>
