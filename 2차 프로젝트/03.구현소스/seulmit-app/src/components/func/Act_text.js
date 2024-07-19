39c41c59cf41fb18659e8c05a2b0dfca10dfb22c
ParkSuMin0611 <sellbell07@gmail.com>
2024-07-19 15:09:55 +0900

    asd

--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/func/Act_text.js
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/func/Act_text.js
@@ -1,62 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState, useEffect } from "react";
 import "../../css/act_text.scss"; // CSS 파일 연결
 import $ from "jquery";
 
 
-function Act_text({ text }) {
-  //text = 외부에서 전달받은 텍스트값
+  function Act_text({ text }) {
 
-  //상태관리변수//////////////////////////////////////////////
-  //애니 상태변수
-  const [isAni, setIsAni] = useState(false);
+    //text : 컴포넌트에서 전달받은 텍스트
+    const [isAni, setIsAni] = useState(false);
+    const [displayedChars, setDisplayedChars] = useState([]);
 
-  const chars = text.replace(/&nbsp;/g, " ").split("");
-  const [displayedChars, setDisplayedChars] = useState([]);
+    const charsRef = useRef(text.replace(/&nbsp;/g, ' ').split('')); // chars를 useRef로 관리
 
-  useEffect(() => {
     $(window).on("scroll", function () {
       const scrollTop = $(this).scrollTop();
       const windowHeight = $(this).height();
-      const triggerPoint = windowHeight / 2;
-
+      const triggerPoint = windowHeight / 2; // 화면 절반 위치
+      
+      // 코드해석: 화면 절반 위치에 스크롤top 이 도달했는데 애니 실행을 안했냐? ㄱ
       if (scrollTop > triggerPoint && !isAni) {
-        setIsAni(true); // 스크롤 위치가 트리거 포인트를 넘어가면 isAni를 true로 변경
-      }
-    });
+        // setIsAni(true);
+  }//if
+
   }, []);
 
-  useEffect(() => {
-    if (isAni) {
+    useEffect(() => {
       let index = 0;
-      const intervalId = setInterval(() => {
+      let intervalId; // intervalId 변수 선언
+  
+      const chars = text.replace(/&nbsp;/g, ' ').split(''); // 염병할 띄어쓰기
+        console.log(chars);
+
+
+      intervalId = setInterval(() => {
         if (index < chars.length) {
           setDisplayedChars((prevChars) => [...prevChars, chars[index]]);
           index++;
-        } else {
-          clearInterval(intervalId);
-        }
-      }, 200); // 200ms 간격으로 한 글자씩 출력
-
+          setIsAni(false);
+        } 
+      },0); // 200ms 간격으로 한 글자씩 출력
+  
       return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 setInterval 해제
-    }
-  }, [isAni]); // chars 의존성 추가
-
-
-  ///코드 리턴구역//////////////////////////////////
-  return (
-    <div className="act-text">
-      {displayedChars.map((char, i) => (
-        <span
-          key={i}
-          className="seulmit-text"
-          style={{ animationDelay: `${i * 0.1}s` }}
-        >
-          {char}
-        </span>
-      ))}
-    </div>
-  );
-}
-
-export default Act_text;
+    }, [isAni,charsRef]); // text를 의존성 배열에 추가
+  
+    return (
+      <div className="act-text">
+        {displayedChars.map((char, i) => (
+          <span
+            key={i}
+            className="seulmit-text"
+            style={{ animationDelay: `${i * 0.1}s` }}
+          >
+            {char}
+          </span>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Act_text;
--- 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Inside.jsx
+++ 2차 프로젝트/03.구현소스/seulmit-app/src/components/pages/Inside.jsx
@@ -46,6 +46,7 @@ function Inside(props) {
         });
       }//if
 
+      
     });
   }, []);
 
