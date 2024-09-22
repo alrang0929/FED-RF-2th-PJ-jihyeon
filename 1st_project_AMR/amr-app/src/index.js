17667016bbf36289a66b16c547fbc41fec536d93
alrang0929 <sellbell07@gmail.com>
2024-09-22 19:31:15 +0900

    scroll top 추가

--- 1st_project_AMR/amr-app/src/index.js
+++ 1st_project_AMR/amr-app/src/index.js
@@ -16,6 +16,7 @@ export default function MainComponent() {
   return (
     <>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ScrollTop />
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Main />} /> 
@@ -30,8 +31,32 @@ export default function MainComponent() {
       </BrowserRouter>
     </>
   );
+
+  
 }
 
+/******************************************* 
+  컴포넌트로 만들고 라우터 안에 넣고
+  라우터 경로변경시 스크롤 최상단이동
+*******************************************/
+const ScrollTop = () => {
+  // 라우터 경로 변경시 path 값 읽어오기
+  // pathname 객체 속성에 담긴다!
+  const { pathname } = useLocation();
+
+  // 화면랜더링 구역에 스크롤상단이동 코드넣기
+  useEffect(() => {
+    // 스크롤 최상단 이동
+    window.scrollTo(0, 0);
+    // 변경된 라우터 경로값 확인
+    // console.log("라우터경로:", pathname);
+  }, [pathname]);
+  // 의존성을 라우터 경로 변수로 설정한다!
+
+  // 컴포넌트 리턴이 필요하나
+  // 소스리턴이 아니므로 null를 쓴다
+  return null;
+}; /////////// ScrollTop 컴포넌트 ////////////
 // fldorxm 객체 생성
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 //출력하기
